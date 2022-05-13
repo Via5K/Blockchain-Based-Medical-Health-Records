b5469500a619f5b0421da6f2d786e865836aee53
Neeraj <2neerajnautiyal@gmail.com>
2022-05-13 20:47:41 +0530

    created .gitignore

--- contracts/records.sol
+++ contracts/records.sol
@@ -3,5 +3,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract records{
+    struct{
+        string lastCheckUpDate;
+        string currentMedicalDosage;
+        string updatedBy; //doctors id
+        string[] currentDiagnosis;
+        string PDFreport;
+    }
+
     
 }
